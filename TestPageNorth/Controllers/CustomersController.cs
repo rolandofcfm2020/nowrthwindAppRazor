e7869dacbf130beb7127a3803c8dfc9b4d201c3f
Rolando <rolando.fcfm@gmail.com>
2020-06-08 22:26:33 -0500

    Changes

--- TestPageNorth/Controllers/CustomersController.cs
+++ TestPageNorth/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestPageNorth.Models;
 
 namespace TestPageNorth.Controllers
 {
@@ -13,6 +14,29 @@ namespace TestPageNorth.Controllers
         {
             ViewBag.Mensaje = "Bienvenido a la lista de clientes";
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult List(CustomersVM model)
+        {
+            DataAccess.NORTHWNDEntities entities = new DataAccess.NORTHWNDEntities();
+            var customerQry = entities.Customers.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(model.SearchText))
+            {
+                customerQry = customerQry.Where(w => w.ContactName.Contains(model.SearchText) || w.Phone.Contains(model.SearchText));
+            }
+
+
+            model.Customers = customerQry.Select(cust => new CustometItemVM
+            {
+                Company = cust.CompanyName,
+                Address = cust.Address + " " + cust.City + " " + cust.Country,
+                Phone = cust.Phone,
+                CustomerName = cust.ContactName
+            }).ToList();
+
+
+            return View(model);
         }
     }
 }
---
+++ TestPageNorth/Models/CustomersVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestPageNorth.Models
+{
+    public class CustomersVM
+    {
+
+        public string SearchText { get; set; }
+        public CustomersVM()
+        {
+            Customers = new List<CustometItemVM>();
+        }
+        public List<CustometItemVM> Customers { get; set; }
+    }
+
+    public class CustometItemVM 
+    {
+        public string Company { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
